41127dc8b416da7ec6c44a454958d4d06fc1348c
Guilherme FGR <guifgr@hotmail.com>
2021-02-02 12:59:17 -0300

    Initial commit

---
+++ Rest/models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rest.models
+{
+    public class Person
+    {
+        public long Id{ get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public string Adress { get; set; }
+        public string Gender { get; set; }
+
+    }
+}
